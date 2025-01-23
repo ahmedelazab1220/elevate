554b668fa95845479ee2634e1a010907ee549685
ahmedelazab1220 <ahmedelazab1220@gmail.com>
2025-01-23 18:07:59 +0200

    feat: initialize Flutter project and add required dependencies

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Elevate());
+}
+
+class Elevate extends StatelessWidget {
+  const Elevate({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+    );
+  }
+}
